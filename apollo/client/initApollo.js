66054d901467a6c6800a268fcb8c8ff446698034
Sergei Droganov <serge@droganov.ru>
2019-02-26 19:09:00 +0300

    Authorization Bearer

--- apollo/client/initApollo.js
+++ apollo/client/initApollo.js
@@ -23,7 +23,7 @@ const httpLink = new HttpLink({
 const authLink = setContext((_, { headers }) => ({
   headers: {
     ...headers,
-    'X-CSRF-Token': localStorage.getItem('token') || '',
+    Authorization: `Bearer ${localStorage.getItem('token') || ''}`,
   },
 }));
 
