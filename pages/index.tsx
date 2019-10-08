e66b14f4edd17f9742f0a89ca896af363aa194b8
Sergei Droganov <serge@droganov.ru>
2019-10-08 21:42:33 +0300

    improvement: add query example
    fetch counries from countries.trevorblades.com, make a proof that SSR works

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,12 @@ import Link from 'next/link';
 import styled from '@emotion/styled';
 import Rocket from '@rkta/entypo/Rocket';
 import { Heading, Button, Divider } from '@rkta/ui';
+import { useQuery } from '@apollo/react-hooks';
 
 import localStatePath from 'path/localStatePath';
 import DefaultLayout from 'layouts/Default';
+
+import COUNTRIES from 'apollo/query/countries.graphql';
 
 const Box = styled.section`
   text-align: center;
@@ -19,27 +22,32 @@ const Box = styled.section`
   }
 `;
 
-const IndexPage = (): React.ReactNode => (
-  <DefaultLayout>
-    <Box>
-      <Rocket size={56} />
-      <Heading baseline level={1}>
-        R-K-T-A
-      </Heading>
-      Poyekhali
-      <Divider verticalSpace={26} />
-      <Link href={localStatePath()}>
-        <Button blockLevel hard>
-          Handle run-time errors
-        </Button>
-      </Link>
-      <Link href="unknown-page">
-        <Button blockLevel hard>
-          Handle routing errors
-        </Button>
-      </Link>
-    </Box>
-  </DefaultLayout>
-);
+const IndexPage = (): React.ReactNode => {
+  const { data } = useQuery(COUNTRIES);
+  return (
+    <DefaultLayout>
+      <Box>
+        <Rocket size={56} />
+        <Heading baseline level={1}>
+          R-K-T-A
+        </Heading>
+        Poyekhali
+        <Divider verticalSpace={26} />
+        <Link href={localStatePath()}>
+          <Button blockLevel hard>
+            Handle run-time errors
+          </Button>
+        </Link>
+        <Link href="unknown-page">
+          <Button blockLevel hard>
+            Handle routing errors
+          </Button>
+        </Link>
+        <Divider verticalSpace={26} />
+        {data && data.countries.map(({ name }: { name: string }) => <div key={name}>{name}</div>)}
+      </Box>
+    </DefaultLayout>
+  );
+};
 
 export default IndexPage;
