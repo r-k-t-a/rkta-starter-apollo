c1060049c01752124db528a0104a5516e019c32f
Павел Брылев <pavel.brylev@supremedesign.ru>
2020-03-06 14:44:51 +0300

    make import graphql types from @ (#121)
    https://app.clubhouse.io/sdlk/story/166/make-import-from-dog

--- scripts/apollo.sh
+++ scripts/apollo.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 GENERATED_PATH=src/@types/apollo-client
-SERVER_SCHEMA_FILE=$GENERATED_PATH/codegen.json
+SERVER_SCHEMA_FILE=$GENERATED_PATH/server-schema.json
+GRAPHQL_TYPES_FILE=$GENERATED_PATH/graphql-types.d.ts
 
 npx apollo client:download-schema $SERVER_SCHEMA_FILE --excludes=src/**/*.{ts,tsx,graphql} > /dev/null
 
-npx apollo client:codegen --localSchemaFile=$SERVER_SCHEMA_FILE --target=typescript --addTypename --outputFlat --watch $GENERATED_PATH
+npx apollo client:codegen --localSchemaFile=$SERVER_SCHEMA_FILE --target=typescript --addTypename --outputFlat --watch $GRAPHQL_TYPES_FILE
